af179e0b99535371f33b1b5f129aac08894be608
Priyanuj Bora <priyanuj1902assam@gmail.com>
2021-04-28 19:18:23 +0530

    finding quotient and remainder manually

--- find_remainder.c
+++ find_remainder.c
@@ -3,7 +3,7 @@
 // CODE IS COMPLETE!!! 
 
 
-int remainder_num(int, int, int);
+int remainder_num(int, int);
 
 int main(void)
 {
@@ -13,12 +13,12 @@ int main(void)
 	scanf("%d-%d", &a, &b);
 	printf("%d/%d: \n", a, b);
 
-	remainder_num(a, b, 0);
+	remainder_num(a, b);
 
 	return 0;
 }
 
-int remainder_num(int a, int b, int dividend)
+int remainder_num(int a, int b)
 {
 	int temp=0, remainder, quotient=0;
 	while(1)
