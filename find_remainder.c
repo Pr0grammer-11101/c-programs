70658ae7ec87f752449747e582c82f05b80ce5aa
Priyanuj Bora <priyanuj1902assam@gmail.com>
2021-04-28 19:14:40 +0530

    finding quotient and remainder manually by using loops

---
+++ find_remainder.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+// CODE IS COMPLETE!!! 
+
+
+int remainder_num(int, int, int);
+
+int main(void)
+{
+	int a, b;
+
+	printf("Enter two numbers: ");
+	scanf("%d-%d", &a, &b);
+	printf("%d/%d: \n", a, b);
+
+	remainder_num(a, b, 0);
+
+	return 0;
+}
+
+int remainder_num(int a, int b, int dividend)
+{
+	int temp=0, remainder, quotient=0;
+	while(1)
+	{
+		if(temp == a)
+		{
+			break;
+		}
+
+		else if(temp > (a-b) && temp < a)
+		{
+			break;
+		}
+		else
+		{
+			temp += b;
+			quotient++;
+			remainder = a-temp;
+		}
+
+
+
+	}
+
+	printf("The quotient is %d and the remainder is %d\n", quotient, remainder);
+
+	return 0;
+}
